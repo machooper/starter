9cba79ef77da1af8a4b8dee878341ad5d6e8acc6
Mac <65713232+machooper@users.noreply.github.com>
2021-06-09 12:18:57 +0100

    feat: :ambulance: Added styled-components to _document.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import Document from "next/document"
+import {ServerStyleSheet} from "styled-components"
 
-export default class SiteDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const sheet = new ServerStyleSheet()
+		const originalRenderPage = ctx.renderPage
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+				})
+
+			const initialProps = await Document.getInitialProps(ctx)
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				)
+			}
+		} finally {
+			sheet.seal()
+		}
+	}
 }
