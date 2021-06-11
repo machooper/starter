c2dff9ab4bcfd26f1fccaf196fdbaf553581c2fc
Mac <65713232+machooper@users.noreply.github.com>
2021-06-11 16:33:26 +0100

    refactor: :recycle: Abstracted SignInContainer to component

--- components/layout/auth.tsx
+++ components/layout/auth.tsx
@@ -69,3 +69,44 @@ export const Profile = () => {
 		)
 	)
 }
+
+const Container = styled.div`
+	height: 90vh;
+	display: grid;
+	place-items: center;
+	text-align: center;
+`
+const LoginContainer = styled.div`
+	${flexColVertCenter}
+`
+const Logo = styled.img``
+const PageTitle = styled.h1`
+	max-width: 400px;
+	text-align: center;
+	margin: 1rem auto;
+`
+
+export const SignInContainer = () => {
+	return (
+		<Container>
+			<LoginContainer>
+				<Logo src='https://global-uploads.webflow.com/5c5add9e3ef45750edad09e8/5c7a49255c2293da0b49aa8a_peg%20logo%20transperant%202-p-500.png' />
+				<PageTitle>Welcome to the Data Dashboard</PageTitle>
+				<LoginButton />
+				<Button
+					border='2px solid #0075db'
+					borderRadius='.5rem'
+					hoverBackgroundColor='#0075db'
+					textColor='#1c1c1c'
+					hoverTextColor='#fff'
+					hoverTransition='.3s ease'
+					label='Support'
+					size='medium'
+					weight={400}
+					margin='0'
+					onClick={() => loginWithRedirect()}
+				/>
+			</LoginContainer>
+		</Container>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,11 @@
 import {useAuth0} from "@auth0/auth0-react"
-import styled from "styled-components"
-import {flexColVertCenter} from "@/styles/mixins"
 import Layout from "@/layout/layout"
-import {LoginButton} from "@/comps/layout/auth"
-import Button from "@/ui/button"
+import {SignInContainer} from "@/comps/layout/auth"
 import Overview from "@/docs/overview"
 
 export default function Home() {
 	const {isAuthenticated} = useAuth0()
-	const Container = styled.div`
-		height: 90vh;
-		display: grid;
-		place-items: center;
-		text-align: center;
-	`
-	const LoginContainer = styled.div`
-		${flexColVertCenter}
-	`
-	const Logo = styled.img``
-	const PageTitle = styled.h1`
-		max-width: 400px;
-		text-align: center;
-		margin: 1rem auto;
-	`
+
 	return (
 		<>
 			{isAuthenticated && (
@@ -33,28 +16,7 @@ export default function Home() {
 					<Overview />
 				</Layout>
 			)}
-			{!isAuthenticated && (
-				<Container>
-					<LoginContainer>
-						<Logo src='https://global-uploads.webflow.com/5c5add9e3ef45750edad09e8/5c7a49255c2293da0b49aa8a_peg%20logo%20transperant%202-p-500.png' />
-						<PageTitle>Welcome to the Data Dashboard</PageTitle>
-						<LoginButton />
-						<Button
-							border='2px solid #0075db'
-							borderRadius='.5rem'
-							hoverBackgroundColor='#0075db'
-							textColor='#1c1c1c'
-							hoverTextColor='#fff'
-							hoverTransition='.3s ease'
-							label='Support'
-							size='medium'
-							weight={400}
-							margin='0'
-							onClick={() => loginWithRedirect()}
-						/>
-					</LoginContainer>
-				</Container>
-			)}
+			{!isAuthenticated && <SignInContainer />}
 		</>
 	)
 }
