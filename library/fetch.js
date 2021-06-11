18f9f88f5da083faa5ffb79b9ca8f5280dcccd86
Mac <65713232+machooper@users.noreply.github.com>
2021-06-11 17:51:40 +0100

    refactor:

--- library/fetch.js
+++ library/fetch.js
@@ -1,6 +1,8 @@
+import {request} from "graphql-request"
 import useSWR from "swr"
 
-const fetchtool = url => {
+// REST API Fetcher
+const restfetch = url => {
 	const fetcher = (...args) => fetch(...args).then(res => res.json())
 	const {data, error} = useSWR(url, fetcher)
 
@@ -11,4 +13,16 @@ const fetchtool = url => {
 	}
 }
 
-export {fetchtool}
+// GraphQL Fetcher
+const graphfetch = (url, query) => {
+	const fetcher = query => request(url, query)
+	const {data, error} = useSWR(query, fetcher)
+
+	return {
+		data,
+		isLoading: !error && !data,
+		isError: error
+	}
+}
+
+export {restfetch, graphfetch}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,9 @@ import {useAuth0} from "@auth0/auth0-react"
 import Layout from "@/layout/layout"
 import {SignInContainer} from "@/comps/layout/auth"
 import Overview from "@/docs/overview"
-import {fetchtool} from "@/lib/fetch"
 
 export default function Home() {
 	const {isAuthenticated} = useAuth0()
-	var res = fetchtool("https://jsonplaceholder.typicode.com/todos/1")
-	console.log(res)
 
 	return (
 		<>
